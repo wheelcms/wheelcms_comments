4a6c9f93b5890f98da846ee47bc86fc2cba9f8c8
Ivo van der Wijk <github@in.m3r.nl>
2013-07-12 16:38:27 +0200

    package created

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='wheelcms_comments',
+      version=version,
+      description="WheelCMS comments package",
+      long_description=open("README.txt").read()
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Ivo van der Wijk',
+      author_email='wheelcms@in.m3r.nl',
+      url='http://github.com/wheelcms/wheelcms_comments',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'pytest',
+      ],
+      entry_points={
+      },
+
+      )
+
