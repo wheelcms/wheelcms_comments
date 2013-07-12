3e527f48bc64ec4c087552d847b49fa58ccd13ac
Ivo van der Wijk <github@in.m3r.nl>
2013-07-12 18:16:48 +0200

    base comment / view template; model defined

--- wheelcms_comments/models.py
+++ wheelcms_comments/models.py
@@ -4,6 +4,43 @@ from django import forms
 from wheelcms_axle.models import Configuration as BaseConfiguration
 from wheelcms_axle.registries.configuration import configuration_registry
 
+from wheelcms_axle.content import type_registry, Content
+from wheelcms_axle.templates import template_registry
+from wheelcms_axle.spoke import Spoke
+from wheelcms_axle.forms import formfactory
+
+class CommentBase(Content):
+    class Meta:
+        abstract = True
+
+    ## gravatar oid?
+    body = models.TextField(blank=False)
+
+class Comment(CommentBase):
+    pass
+
+class CommentForm(formfactory(Comment)):
+    pass
+    ## captcha
+
+    ## body doesn't have to be rich
+
+
+class CommentType(Spoke):
+    implicit_add = False   # can't create through frontend directly
+    add_to_index = False   # do not index in search
+    discussable = False    # well...
+
+    model = Comment
+    form = CommentForm
+
+    title = "A comment"
+    # type_icon = icon = ..
+
+template_registry.register(CommentType, "wheelcms_comments/comment_view", "Comment View", default=True)
+
+type_registry.register(CommentType)
+
 class Configuration(models.Model):
     main = models.ForeignKey(BaseConfiguration, related_name="comments")
     enabled = models.BooleanField(default=True)
